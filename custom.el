bdb9de9ed46508034d7ccf928085f22f65923b59
Nelson Loyola <nloyola@gmail.com>
2015-10-30 13:12:00 -0600

    Added elcim function
    Launches a run configuration in the *compilation* buffer.

--- custom.el
+++ custom.el
@@ -10,6 +10,12 @@
            (quote
             (lambda nil
               (interactive)
+              (nl/eclim-run-configuartion "Run PlateDecoder"))))
+     (eval global-set-key
+           [f5]
+           (quote
+            (lambda nil
+              (interactive)
               (eclim-java-run-run "Run PlateDecoder"))))
      (eval setq eclimd-default-workspace
            (concat
@@ -22,4 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ac-emacs-eclim-candidate-face ((t (:inherit ac-candidate-face))))
+ '(ac-emacs-eclim-selection-face ((t (:inherit ac-selection-face))))
+ '(mode-line ((t (:foreground "#030303" :background "#6b6b6b" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#6b6b6b" :box nil)))))
