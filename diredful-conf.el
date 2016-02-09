35939759a95f54ee150f35b3c30d9c8969910007
Nelson Loyola <nloyola@gmail.com>
2016-02-08 22:43:47 -0700

    Added diredful

---
+++ diredful-conf.el
@@ -0,0 +1,4 @@
+
+("javascript-files" "html-files" "markdown-files" "scala-files")
+
+(("javascript-files" (:foreground "lime green") "js" nil nil nil nil) ("html-files" (:foreground "light salmon") "html" nil t nil nil) ("markdown-files" (:foreground "IndianRed2") "md" nil nil nil nil) ("scala-files" (:foreground "SteelBlue3") "scala sbt" nil nil nil nil))
