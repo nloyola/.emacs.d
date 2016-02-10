a60da634675aaa0b9789209c451fa86df0b1b0d7
Nelson Loyola <nloyola@gmail.com>
2016-02-09 20:07:19 -0700

    Added config fo Hi Lock

--- custom.el
+++ custom.el
@@ -3,12 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t)
+ '(js2-highlight-level 3)
  '(package-selected-packages
    (quote
     (zeal-at-point whole-line-or-region visual-regexp-steroids use-package undo-tree transpose-frame smooth-scrolling smartscan smartparens shackle scratch sbt-mode revive rainbow-delimiters powerline noflet monokai-theme markdown-mode magit macrostep key-chord js2-mode hydra hungry-delete helm-swoop helm-projectile helm-gtags helm-flx helm-c-yasnippet helm-ag guide-key groovy-mode gradle-mode goto-last-change git-timemachine ggtags framemove flycheck fic-mode expand-region emacs-eclim edit-server company color-identifiers-mode beacon ag ace-window)))
  '(safe-local-variable-values
    (quote
-    ((eval global-set-key
+    ((eval setq require-final-newline nil)
+     (eval global-set-key
            [f5]
            (quote sbt-command))
      (eval setq-default indent-tabs-mode nil)
