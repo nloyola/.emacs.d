06ebfc1272cac797694ab2c155c1fa5d42955f57
Nelson Loyola <nloyola@gmail.com>
2016-05-26 19:31:00 -0600

    More config
    1. Tweaked snippet for angular components.
    2. Tweaked emacs functions that manipulate frames.

--- users/obelix/window_config.el
+++ users/obelix/window_config.el
@@ -1,4 +1,4 @@
-(defun nl/set-size-and-position ()
+(defun nl/main-frame-set-size-and-position ()
   "Set the size and position of the Emacs window."
   (interactive)
   (let ((frame (selected-frame)))
@@ -6,23 +6,23 @@
     (set-frame-size frame 229 (/ (x-display-pixel-height) (frame-char-height)))
     ))
 
-(defun nl/set-frame-size-and-position ()
+(defun nl/frame-set-size-and-position ()
   "Set the size and position of the Emacs window."
   (interactive)
   (let ((frame (selected-frame)))
-    (nl/set-frame-size-and-position-hook frame)
+    (nl/frame-set-size-and-position-hook frame)
     )
   )
 
-(defun nl/set-frame-size-and-position-hook (frame)
+(defun nl/frame-set-size-and-position-hook (frame)
   (set-frame-position frame 2200 60)
   (set-frame-size frame 120 (floor (* (/ (x-display-pixel-height) (frame-char-height)) 0.80)))
   )
 
-(add-hook 'after-make-frame-functions 'nl/set-frame-size-and-position-hook t)
+(add-hook 'after-make-frame-functions 'nl/frame-set-size-and-position-hook t)
 
 (add-hook 'window-setup-hook (lambda ()
-                               (nl/set-size-and-position)
+                               (nl/main-frame-set-size-and-position)
                                (resume)
                                (make-frame-command)
                                ))
