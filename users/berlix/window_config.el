656ed500fa9da133ada9847a4a3778f1e1c9d2c6
Nelson Loyola <nloyola@gmail.com>
2016-07-26 13:58:39 +0200

    Using anzu mode
    Added config for berlix desktop comp.

---
+++ users/berlix/window_config.el
@@ -0,0 +1,28 @@
+(defun nl/main-frame-set-size-and-position ()
+  "Set the size and position of the Emacs window."
+  (interactive)
+  (let ((frame (selected-frame)))
+    (set-frame-position frame -1 0)
+    (set-frame-size frame 229 (/ (x-display-pixel-height) (frame-char-height)))
+    ))
+
+(defun nl/frame-set-size-and-position ()
+  "Set the size and position of the Emacs window."
+  (interactive)
+  (let ((frame (selected-frame)))
+    (nl/frame-set-size-and-position-hook frame)
+    )
+  )
+
+(defun nl/frame-set-size-and-position-hook (frame)
+  (set-frame-position frame 1920 60)
+  (set-frame-size frame 120 (floor (* (/ (x-display-pixel-height) (frame-char-height)) 0.80)))
+  )
+
+(add-hook 'after-make-frame-functions 'nl/frame-set-size-and-position-hook t)
+
+(add-hook 'window-setup-hook (lambda ()
+                               (nl/main-frame-set-size-and-position)
+                               (resume)
+                               (make-frame-command)
+                               ))
