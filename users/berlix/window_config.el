ebe60fa3eefb58698a9d75488591ab6a5d4d442c
Nelson Loyola <nloyola@gmail.com>
2018-08-15 11:53:00 +0200

    more config for window / frame on 4k monitor

--- users/berlix/window_config.el
+++ users/berlix/window_config.el
@@ -32,26 +32,36 @@
                       (/ (x-display-pixel-height) (frame-char-height)))
       (set-face-attribute 'default frame :font "Ubuntu Mono" :height 180 :weight 'medium)))))
 
-(defun nl/frame-set-size-and-position ()
-  "Set the size and position of the Emacs window."
+(defun nl/new-frame ()
+  "Create a new frame on the 4k display."
   (interactive)
-  (let ((frame (selected-frame)))
-    (nl/frame-set-size-and-position-hook frame)))
+  (let ((frame (make-frame)))
+    (select-frame-set-input-focus frame)
+    (switch-to-buffer "*scratch*")
+    (set-frame-position frame 2200 120)
+    (set-face-attribute 'default frame :font "Ubuntu Mono" :height 180 :weight 'medium)
+    (set-frame-size frame
+                    (* 100 (frame-char-width frame))
+                    (* 70(frame-char-height frame))
+                    t)))
 
-(defun nl/frame-set-size-and-position-hook (frame)
-  (set-frame-position frame -1 0)
-  (set-frame-size frame
-                  (/ (x-display-pixel-width) (frame-char-width))
-                  (floor (* (/ (x-display-pixel-height) (frame-char-height)) 0.80)))
-  (set-face-attribute 'default frame :font "Ubuntu Mono" :height 110 :weight 'medium))
-
-(add-hook 'after-make-frame-functions 'nl/frame-set-size-and-position-hook t)
+(defun nl/frame-3rd-display ()
+  "Create a new frame on the 3rd display."
+  (interactive)
+  (let ((frame (make-frame)))
+    (select-frame-set-input-focus frame)
+    (switch-to-buffer "*scratch*")
+    (set-frame-position frame -1 0)
+    (set-face-attribute 'default frame :font "Ubuntu Mono" :height 110 :weight 'medium)
+    (set-frame-size frame
+                    (/ (x-display-pixel-width frame) (frame-char-width frame))
+                    (floor (* (/ (x-display-pixel-height frame) (frame-char-height frame)) 0.80)))))
 
 (defun nl/window-setup-hook ()
   (let ((frame (selected-frame)))
     (nl/main-frame-set-size-and-position)
     (resume)
-    (make-frame-command)
+    (nl/frame-3rd-display)
     (select-frame-set-input-focus frame)))
 
 (add-hook 'window-setup-hook 'nl/window-setup-hook)
