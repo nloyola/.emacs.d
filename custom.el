d4a7959c895c0aeae329c494a31ed75ac3c11333
Nelson Loyola <nloyola@gmail.com>
2019-02-20 15:42:06 +0100

    Fixed side windows configuration

--- custom.el
+++ custom.el
@@ -14,7 +14,11 @@
     (scala-mode php-mode zop-to-char yaml-mode web-mode wand visual-regexp bm company-tern tern super-save spaceline slime org-bullets protobuf-mode lorem-ipsum less-css-mode karma js2-refactor counsel swiper ivy-hydra ivy highlight-indent-guides helm grunt emr company-emacs-eclim dumb-jump diredful clipmon zeal-at-point whole-line-or-region visual-regexp-steroids use-package undo-tree transpose-frame smooth-scrolling smartscan smartparens shackle scratch sbt-mode revive rainbow-delimiters powerline noflet monokai-theme markdown-mode magit macrostep key-chord js2-mode hydra hungry-delete helm-swoop helm-projectile helm-gtags helm-flx helm-c-yasnippet helm-ag guide-key groovy-mode gradle-mode goto-last-change git-timemachine ggtags framemove flycheck fic-mode expand-region emacs-eclim edit-server company color-identifiers-mode beacon ag ace-window)))
  '(safe-local-variable-values
    (quote
-    ((org-edit-src-content-indentation . 0)
+    ((eval progn
+           (use-package angular :load-path "~/.emacs.d")
+           (setq dumb-jump-default-project
+                 (projectile-project-root)))
+     (org-edit-src-content-indentation . 0)
      (org-src-preserve-indentation)
      (eval progn
            (use-package bbweb-project :load-path "~/.emacs.d"))))))
