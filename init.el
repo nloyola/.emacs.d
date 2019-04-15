7e0ddb231485cd659f7a55fc1ee9a155b45f16a5
Nelson Loyola <nloyola@gmail.com>
2019-04-15 16:00:12 +0200

    No longer using MELPA stable
    Caused too many package version conflicts.

--- init.el
+++ init.el
@@ -40,10 +40,10 @@
 (require 'package)
 (setq package-archives
       '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
-        ("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ;;("MELPA Stable" . "https://stable.melpa.org/packages/")
         ("MELPA"        . "https://melpa.org/packages/"))
       package-archive-priorities
-      '(("MELPA Stable" . 10)
+      '(;;("MELPA Stable" . 10)
         ("GNU ELPA"     . 5)
         ("MELPA"        . 0)))
 
