ccf1d3ab70e4dcbaf77dd846a54e05d5c7fca6f0
Nelson Loyola <nloyola@gmail.com>
2020-02-24 10:37:51 +0100

    Using new computer
    Named it obelix (the Swedish version) since Canadian obelix is no more.

--- users/obelix/window_config.el
+++ users/obelix/window_config.el
@@ -1,27 +1,107 @@
+(defun nl/monitor-pixel-dimensions (monitor)
+  (let* ((monitor-attributes (display-monitor-attributes-list))
+         (num-displays (length (display-monitor-attributes-list))))
+    (if (< monitor num-displays)
+        (list (nth 3 (assq 'geometry (nth monitor monitor-attributes)))
+              (nth 4 (assq 'geometry (nth monitor monitor-attributes))))
+      (error "invalid monitor number: %d" monitor))))
+
+(defun nl/monitor-pixel-width (monitor)
+  (car (nl/monitor-pixel-dimensions monitor)))
+
+(defun nl/monitor-pixel-height (monitor)
+  (nth 1 (nl/monitor-pixel-dimensions monitor)))
+
 (defun nl/main-frame-set-size-and-position ()
   "Set the size and position of the Emacs window."
   (interactive)
-  (let ((frame (selected-frame)))
-    (set-frame-position frame -1 0)
-    (set-frame-size frame 233 (/ (x-display-pixel-height) (frame-char-height)))))
+  (let* ((frame (selected-frame))
+         (num-displays (length (display-monitor-attributes-list)))
+         (desired-width-in-chars 235)
+         (desired-width-in-pixels (* desired-width-in-chars (frame-char-width))))
+    (set-face-attribute 'default frame :font "Fira Code Medium-12")
+    (cond
+     ((eq num-displays 2)
+      (set-frame-size frame
+                      desired-width-in-chars
+                      (/ (nl/monitor-pixel-height 1) (frame-char-height)))
+      ;; cannot use negative value for X since it is not placed in correct location on startup
+      (set-frame-position frame
+                          (- (+ (nl/monitor-pixel-width 0) (nl/monitor-pixel-width 1))
+                             desired-width-in-pixels)
+                          0))
+     ((eq num-displays 3)
+      (let* ((window-frame-in-pixels 16))
+        ;; cannot use negative value for X since it is not placed in correct location on startup
+        (set-frame-position frame
+                            (- (+ (nl/monitor-pixel-width 0) (nl/monitor-pixel-width 1))
+                               window-frame-in-pixels
+                               desired-width-in-pixels)
+                            0)
+        (set-frame-size frame
+                        desired-width-in-chars
+                        (/ (nl/monitor-pixel-height 1) (frame-char-height))))))))
 
-(defun nl/frame-set-size-and-position ()
-  "Set the size and position of the Emacs window."
+(defun nl/new-frame ()
+  "Create a new frame on the 4k display."
   (interactive)
+  (let* ((frame (make-frame)))
+    (select-frame-set-input-focus frame)
+    (switch-to-buffer (current-buffer))
+    (set-frame-position frame (+ (nl/monitor-pixel-width 1) 400) 120)
+    (set-face-attribute 'default frame :font "Fira Code Medium-12")
+    (set-frame-size frame
+                    (* 120 (frame-char-width frame))
+                    (* 70 (frame-char-height frame))
+                    t)))
+
+(defun nl/frame-2nd-display ()
+  "Create a new frame on the 3rd display."
+  (interactive)
+  (let ((frame (make-frame))
+        (monitor-pixel-width (nth 3 (assq 'geometry (nth 0 (display-monitor-attributes-list)))))
+        (monitor-pixel-height (nth 4 (assq 'geometry (nth 0 (display-monitor-attributes-list))))))
+    (select-frame-set-input-focus frame)
+    (set-face-attribute 'default frame :font "Fira Code Medium-12")
+    (switch-to-buffer "*scratch*")
+    (set-frame-position frame
+                        (/ (nl/monitor-pixel-width 0) 2)
+                        0)
+    (set-frame-size frame
+                    (/ (/ (nl/monitor-pixel-width 0) (frame-char-width frame)) 2)
+                    (floor (* (/ (nl/monitor-pixel-height 0) (frame-char-height frame)) 0.97)))))
+
+(defun nl/window-setup-hook ()
   (let ((frame (selected-frame)))
-    (nl/frame-set-size-and-position-hook frame)
-    )
-  )
+    (nl/main-frame-set-size-and-position)
+    ;;(resume)
+    (select-frame-set-input-focus frame)))
 
-(defun nl/frame-set-size-and-position-hook (frame)
-  (set-frame-position frame 2200 60)
-  (set-frame-size frame 120 (floor (* (/ (x-display-pixel-height) (frame-char-height)) 0.80)))
-  )
+(add-hook 'window-setup-hook 'nl/window-setup-hook)
 
-(add-hook 'after-make-frame-functions 'nl/frame-set-size-and-position-hook t)
+;; Search specified tree for the item with the specified key
+;; http://stackoverflow.com/questions/11912027/emacs-lisp-search-anything in-a-nested-list
+(defun tree-assoc (key tree)
+  (when (consp tree)
+    (destructuring-bind (x . y) tree
+  (if (eql x key) tree
+    (or (tree-assoc key x) (tree-assoc key y))))))
 
-(add-hook 'window-setup-hook (lambda ()
-                               (nl/main-frame-set-size-and-position)
-                               (resume)
-                               (make-frame-command)
-                               ))
+;; adjust the font size based on the monitor's pixel height
+;; http://arnab-deka.com/posts/2012/09/emacs-change-fonts-dynamically-based-on-screen-resolution
+(defun fontify-frame (&optional frame)
+  (interactive)
+  (or frame (setq frame (selected-frame)))
+  (if window-system
+      (let ((monitor-pixel-height (nth 4 (assq 'geometry (frame-monitor-attributes frame)))))
+        (if (<= monitor-pixel-height 1080)
+            (set-frame-parameter 'nil 'font "Fira Code Medium-9")
+          (set-frame-parameter 'nil 'font "Fira Code Medium-12")))))
+
+;;(push
+;; 'fontify-frame after-make-frame-functions)
+
+;;(run-with-idle-timer 2 t 'fontify-frame 'nil)
+
+;; 0      1         2         3         4         5         6        7          8         9         0
+;; 456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
