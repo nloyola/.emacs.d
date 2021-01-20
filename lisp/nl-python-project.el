0246fc2e9c729b510785c8560d95b9e450fcf3b2
Nelson Loyola <nloyola@gmail.com>
2021-01-20 12:22:32 +0100

    Added project config for python

---
+++ lisp/nl-python-project.el
@@ -0,0 +1,29 @@
+;;; nl-python-project --- package to help with development in Python projects
+
+;;; Commentary:
+
+;; Settings and functions to support software development.
+
+;;; Code:
+
+(provide 'nl-python-project)
+
+(defvar python-mode-map)
+
+(message "------------------> here")
+
+(defhydra hydra-nl-python-scala (:hint nil)
+  "Project"
+  ("a" hydra-nl-align/body "align" :color blue :column "Python")
+  ("i" nl/indent-whole-buffer "indent buffer" :color blue)
+  ("x" lsp-find-references "find references" :color blue)
+  ("p" dumb-jump-go-prompt "prompt" :color blue))
+
+;; this def uses a lambda to show that it is possible, id does not need to use it
+(key-chord-define python-mode-map "jc" '(lambda () (interactive)
+                                         (hydra-nl-python-scala/body)))
+
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
